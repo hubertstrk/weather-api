13fcd7ca5b809271c5fc88b8dba5285cec11a76a
Hubert Stärk <hubert.staerk@gmail.com>
2024-10-30 23:38:28 +0100

    add weather model

--- src/app/weather-widget/weather-service.ts
+++ src/app/weather-widget/weather-service.ts
@@ -1,20 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { take, map, Observable } from 'rxjs';
+import { WeatherForecats } from './model';
 
 @Injectable({ providedIn: 'root' })
 export class WeatherService {
   constructor(private httpClient: HttpClient) {}
 
-  getWeatherData(): Observable<any> {
+  getWeatherData(): Observable<WeatherForecats> {
     return this.httpClient
-      .get(
+      .get<WeatherForecats>(
         'https://api.openweathermap.org/data/2.5/forecast?q=London&appid=539fdbc15ab95b61ca0df10dc5b07f58'
       )
       .pipe(
         take(1),
-        map((data: any) => {
-          return data;
+        map(response => {
+          response.list.map(item => {
+            const foo = new Date(item.dt_txt).toLocaleString();
+            console.info(foo);
+          });
+
+          return response;
         })
       );
   }
